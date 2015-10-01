8831e776cf09d7e12420b7451d1400ceb9eda787
Austin Mead <austinmead92@aol.com>
2015-10-01 12:05:45 -0500

    Movie quotes

---
+++ PlayMovieQuotes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayMovieQuotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string varURL = "";
+            SoundPlayer quote = new SoundPlayer();
+            Random randNum = new Random();
+            int selectedQuote = (randNum.Next(1, 3));
+            switch (selectedQuote)
+            {
+                case 1:
+                    varURL = "http://www.wavsource.com/snds_2015-09-20_4380281261564803/tv/simpsons/apu/bastard.wav";
+                    break;
+                case 2:
+                    varURL = "http://www.wavsource.com/snds_2015-09-20_4380281261564803/tv/simpsons/apu/do_not_listen.wav";
+                    break;
+                case 3:
+                    varURL = "http://www.wavsource.com/snds_2015-09-20_4380281261564803/tv/simpsons/apu/hell.wav";
+                    break;
+                default:
+                    break;
+            }
+            quote.SoundLocation = varURL;
+           
+            try
+            {
+                quote.Play();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(@"i'm sorry, you are not connected to the internet. please reconnect and try again.");
+            }
+
+            Console.ReadLine();
+
+            
+          
+
+
+    
+
+        }
+        
+    }
+}
